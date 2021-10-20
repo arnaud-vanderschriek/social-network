2f1c8bfeabb7273a2b202d228826504bae9f412c
arnoo <arnaud>
2021-10-20 21:47:29 +0200

    MongoDB setup (config folder => db.js), Routing setup (routes folder => user.routes.js)

--- back/server.js
+++ back/server.js
@@ -1,7 +1,16 @@
-const express = require('express'); 
-require('dotenv').config({path: './config/.env'})
+const express = require('express');
 const app = express();
+const userRoutes = require('./routes/user.routes.js')
+require('dotenv').config({path: './config/.env'})
+require('./config/db')
 
+
+
+
+//routes
+app.use('/api/user', userRoutes)
+
+// server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`)
 })

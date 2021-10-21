66bbd614683100d00f4aafa73e76231e3fb77715
arnoo <arnaud>
2021-10-21 20:38:49 +0200

    add in user.controller: getAllUsers, userInfo, findById functions + routes

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router(); 
 const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
 
+//auth controller
 router.post('/register', authController.signUp)
+
+//user controller
+router.get('/', userController.getAllUsers)
+router.get('/:id', userController.userInfo)
 
 module.exports = router

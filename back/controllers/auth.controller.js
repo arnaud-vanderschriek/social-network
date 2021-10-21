84bba313776a4c3e80a73d0429a5601da3b95f17
arnoo <arnaud>
2021-10-21 20:42:49 +0200

    remove console.log + code visibility

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user.model');
 
+
 module.exports.signUp = async (req, res) => {
   const { pseudo, email, password } = req.body
 
--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -1,13 +1,14 @@
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
+
 module.exports.getAllUsers = async (req, res) => {
   const users = await UserModel.find().select('-password'); 
   res.status(200).json(users)
 }
 
+
 module.exports.userInfo = async (req, res) => {
-  console.log(req.params);
   if (!ObjectID.isValid(req.params.id)) 
     return res.status(400).send('ID unknown : ' + req.params.id)
 

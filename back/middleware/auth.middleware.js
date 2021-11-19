244da877378627a35f8d78ea59a9de66f60f13f0
arnaud-vanderschriek <arnaud>
2021-11-19 16:10:25 +0100

    add cookie-parser,add middleware folder, add checkUser function to verify token from client, add requireAuth function

---
+++ back/middleware/auth.middleware.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/user.model");
+
+module.exports.checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        res.cookie("jwt", "", { maxAge: 1 });
+        next();
+      } else {
+        let user = await UserModel.findById(decodedToken);
+        res.locals.user = user;
+        console.log(user);
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports.requireAuth = (req, res) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      try {
+        console.log(decodedToken.id)
+        next();
+      } catch (err) {
+        console.log(err)
+      }
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(decodedToken.id);
+        next();
+      }
+    });
+  } else {
+    console.log("No token");
+  }
+};
--- back/server.js
+++ back/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const userRoutes = require("./routes/user.routes.js");
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(express.json());
@@ -10,6 +12,13 @@ app.use(
     extended: true,
   })
 );
+app.use(cookieParser());
+
+//jwt
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 
 //routes
 app.use("/api/user", userRoutes);
